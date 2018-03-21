d816a6ef0df027a4d740521c4d9846f2c8b1deb7
Mehdi <mehdi@mehdi-aberkane.fr>
2018-03-21 09:56:05 +0100

    add edit to todo

--- Exo 02/correction/main.js
+++ Exo 02/correction/main.js
@@ -12,7 +12,7 @@ function getAllLocal() {
   for (var variable in datas) {
     if (datas.hasOwnProperty(variable)) {
       document.getElementById("listing").innerHTML += '<li class="list-group-item"><label>'+datas[variable]+'</label>' +
-          '<input type="button" onclick="deleteAction('+variable+')" name="name" class="btn delete btn-danger pull-right" value="Remove"></li>';
+          '<input type="button" onclick="deleteAction('+variable+')" name="name" class="btn delete btn-danger pull-right" value="Remove"><input type="button" onclick="editAction('+variable+')" name="name" class="btn editAction btn-primary pull-right" value="Edit"></li>';
     }
   }
 })();
@@ -21,7 +21,12 @@ document.getElementById("checkAll").onsubmit = function(e){
   e.stopPropagation();
   e.preventDefault();
   var newTodo = document.getElementById("todo").value;
-  localStorage.setItem(new Date().valueOf(), newTodo);
+  if (document.getElementById("key").value) {
+    localStorage.setItem(document.getElementById("key").value, newTodo);
+  } else {
+    localStorage.setItem(new Date().valueOf(), newTodo);
+  }
+  
   document.getElementById("todo").value = null;
   window.location.reload();
 };
@@ -29,4 +34,10 @@ document.getElementById("checkAll").onsubmit = function(e){
 function deleteAction(idTodo){
   localStorage.removeItem(idTodo);
   window.location.reload()
+}
+
+function editAction(idTodo){
+  let todo = localStorage.getItem(idTodo);
+  document.getElementById("todo").value = todo;
+  document.getElementById("key").value = idTodo;
 }
